dfbabff4a822bf5b35f6e618655ec81ba1dbd25c
Yvette Vargas <yrvargas@uci.edu>
2025-03-14 22:35:22 +0000

    updated printing ladder format

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -76,11 +76,11 @@ void load_words(set<string> & word_list, const string& file_name) {
 
 void print_word_ladder(const vector<string>& ladder) {
 	if (ladder.empty()) {
-		cerr << "ERROR: no ladder found";
+		cout << "No word ladder found." << endl;
 		return;
 	}
 	for (string word : ladder) {
-		cout << word << " -> ";
+		cout << word << " ";
 	}
 	cout << endl;
 }
